6539f4150a8083bf3cbc30acd1f3a05b60f880ff
Zephrax <Z3PHR4X@users.noreply.github.com>
2023-06-28 21:01:53 +0200

    Added assets for Toolkit
    Still contains some redundant files, needs cleanup

---
+++ Assets/Scripts/CollisionSFX.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSFX : MonoBehaviour
+{
+    public AudioClip sfx;
+    private AudioSource sfxSource;
+
+    private void Start()
+    {
+        sfxSource.playOnAwake = false;
+        sfxSource.loop = false;
+        
+        sfxSource.clip = sfx;
+        sfxSource.volume = 0.2f;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        sfxSource.Play();
+    }
+}
